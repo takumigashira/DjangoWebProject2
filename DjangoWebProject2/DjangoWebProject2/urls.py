3ce444405597593455fb5ba3b2f49572e75999c5
takumigashira <takumigashira@gmail.com>
2022-07-19 15:02:27 +0900

    init

--- DjangoWebProject2/DjangoWebProject2/urls.py
+++ DjangoWebProject2/DjangoWebProject2/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
+import HelloMyApp.views
 
 # Uncomment next two lines to enable admin:
 #from django.contrib import admin
 #from django.urls import path
 
 urlpatterns = [
+    url(r'^$', HelloMyApp.views.index, name='index'),
+    url(r'^home$', HelloMyApp.views.index, name='home'),
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
 ]
---
+++ DjangoWebProject2/HelloMyApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HelloMyAppConfig(AppConfig):
+    name = 'HelloMyApp'
