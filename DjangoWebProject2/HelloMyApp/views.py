a44ef5bfa8db62ca1000eed332ff69e36635259f
takumigashira <takumigashira@gmail.com>
2022-07-19 15:53:24 +0900

    second

--- DjangoWebProject2/HelloMyApp/views.py
+++ DjangoWebProject2/HelloMyApp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello Django!")
+    now = datetime.now()
+
+    html_content = "<html><head><title>Hello, Django</title></head><body>"
+    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+
+    return HttpResponse(html_content)
