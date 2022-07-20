71762443a2ededa6f441ddee143a1d5832fcf069
takumigashira <takumigashira@gmail.com>
2022-07-20 14:23:32 +0900

    add template

--- DjangoWebProject2/HelloMyApp/views.py
+++ DjangoWebProject2/HelloMyApp/views.py
@@ -1,3 +1,4 @@
+from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
@@ -5,8 +6,12 @@ from datetime import datetime
 def index(request):
     now = datetime.now()
 
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
-
-    return HttpResponse(html_content)
+    return render(
+        request,
+        "HelloMyApp/index.html",
+            {
+                'title': "Hello MyApp",
+                'message': "Hello Django!",
+                'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+            }
+        )
